3c291cb62e84a131fed80920ff66ffcd216660df
adriansamir300au-afk <adrian.samir300au@gmail.com>
2026-05-22 21:26:49 -0500

    Update ejercicio4.py

--- parte2_estructurados/ejercicio4.py
+++ parte2_estructurados/ejercicio4.py
@@ -2,10 +2,19 @@
 # EJERCICIO 4: TUPLAS
 # Asignado a: Adrian Ruesta
 # ==============================================================================
+#Lenguajes de programación
+LENGUAJES=("JAVA","PYTHON","SQL","HTML","C++")
+#Mostrar todos los elementos
+print("5 Lenguajes de Programación")
+print(LENGUAJES)
+#Mostrar el primer elemento
+print("Primer Lenguaje mencionado")
+print(LENGUAJES[0])
+#Mostrar el último elemento
+print("Ultimo Lenguaje mencionado")
+print(LENGUAJES[4])
+#Contar la cantidad de elementos
+print("¿Cuantos lenguajes de programacón hay en la TUPLE?")
+cantidad_total=len(LENGUAJES)
+print("En la TUPLE hay",cantidad_total," lenguajes de programación.")
 
-def main():
-    print("--- EJERCICIO 4: TUPLAS ---")
-    # TODO: Desarrolla tu código aquí
-    
-if __name__ == "__main__":
-    main()
